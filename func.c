1e85d92ef5fdf15670d657296e87c16039232677
kakukakujirori <kakuda40611@gmail.com>
2020-05-16 18:49:37 +0900

    add func.c

---
+++ func.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int hi() {printf("Hello\n");}
+
+int add_print2(int a0, int a1) {printf("%d\n", a0 + a1);}
+
+int add_2(int a0, int a1) {return a0 + a1;}
+
+int add_print3(int a0, int a1, int a2) {printf("%d\n", a0 + a1 + a2);}
+
+int add_3(int a0, int a1, int a2) {return a0 + a1 + a2;}
+
+int add_print4(int a0, int a1, int a2, int a3) {printf("%d\n", a0 + a1 + a2 + a3);}
+
+int add_4(int a0, int a1, int a2, int a3) {return a0 + a1 + a2 + a3;}
+
+int add_print5(int a0, int a1, int a2, int a3, int a4) {
+    printf("%d\n", a0 + a1 + a2 + a3 + a4);
+}
+
+int add_5(int a0, int a1, int a2, int a3, int a4) {
+    return a0 + a1 + a2 + a3 + a4;
+}
+
+int add_print6(int a0, int a1, int a2, int a3, int a4, int a5) {
+    printf("%d\n", a0 + a1 + a2 + a3 + a4 + a5);
+}
+
+int add_6(int a0, int a1, int a2, int a3, int a4, int a5) {
+    return a0 + a1 + a2 + a3 + a4 + a5;
+}
+
+int add_print7(int a0, int a1, int a2, int a3, int a4, int a5, int a6) {
+    printf("%d\n", a0 + a1 + a2 + a3 + a4 + a5 + a6);
+}
+
+int add_7(int a0, int a1, int a2, int a3, int a4, int a5, int a6) {
+    return a0 + a1 + a2 + a3 + a4 + a5 + a6;
+}
